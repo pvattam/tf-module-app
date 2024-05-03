92dc124af046de679692ce6d224988ce2deb6912
Pushpak Vattam <vattampushpak243@gmail.com>
2024-05-03 07:32:42 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 ##!/bin/bash
 #
 
-ansible-pull -i localhost, -U https://github.com/pvattam/roboshop-ansible.git roboshop.yml -e role_name=roboshop -e env=${env} | tee -a /opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/pvattam/roboshop-ansible.git roboshop.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
